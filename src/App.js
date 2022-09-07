6c73ede9eaf8502530f9cbc79ff9ca28cf8089bb
Niniatura <neli.marcos@gmail.com>
2022-09-07 17:49:45 -0300

    nav bar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import NavBar from './components/navBar/NavBar';
 
 function App() {
   return (
-    <div className="App">
-      <h1 class="text-3xl shadow-md font-bold underline">
-      Hello world!
-      </h1>
-    </div>
+    <>
+      <NavBar />
+    </>
   );
 }
 
