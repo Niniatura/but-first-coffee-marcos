71ce8970614444791723389c6e76db91d35b3f13
Niniatura <neli.marcos@gmail.com>
2022-09-08 11:38:22 -0300

    Nav con bg color y hover

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import NavBar from './components/navBar/NavBar';
 function App() {
   return (
     <>
+    <div className="min-h-screen bg-gray-300">
       <NavBar />
+    </div>
     </>
   );
 }
--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -6,13 +6,13 @@ const NavBar = () => {
         <nav className="flex flex-row items-center mobile:flex-col tablet:flex-row">
             <img src="./images/Logo/26.jpeg" alt="Logo" className="logo w-20 m-2"/>
             <ul className="lista flex tablet:flex-row tablet:justify-around mobile:flex-col mobile:text-center">
-                <li>Inicio</li>
-                <li>Todos los productos</li>
-                <li>Cafe</li>
-                <li>Cold Brew</li>
-                <li>Cafeteras</li>
-                <li>Contacto</li>
-                <li>Login</li>
+                <li className="hover:text-white">Inicio</li>
+                <li className="hover:text-white">Todos los productos</li>
+                <li className="hover:text-white">Cafe</li>
+                <li className="hover:text-white">Cold Brew</li>
+                <li className="hover:text-white">Cafeteras</li>
+                <li className="hover:text-white">Contacto</li>
+                <li className="hover:text-white">Login</li>
             </ul>
         </nav>
     </>
