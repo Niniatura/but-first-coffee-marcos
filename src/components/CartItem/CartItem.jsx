89e29085d5f796d2e1f8a990cd739c6f6d79ba18
Niniatura <neli.marcos@gmail.com>
2022-10-28 12:01:56 -0300

    Cart view styles

--- src/components/CartItem/CartItem.jsx
+++ src/components/CartItem/CartItem.jsx
@@ -7,46 +7,35 @@ function CartItem({props}) {
 
     return (
         <>
-            {/* <div className="cart-item mobile:w-80 h-auto flex items-center bg-gray-600 rounded-md">
-                <img className="m-2 w-20 rounded-lg object-scale-down flex mt-3 justify-start" src={props.img} alt="product" />
-                <div className='grilla-contenido space-y-4'>
-                    <div className='grilla grid grid-rows-2 grid-flow-col gap-x-28 ml-10 mb-10'>
-                        <span className=' text-gray-900 dark:text-gray-200 max-h-10'>Producto</span>
-                            <span className="text-sm font-semibold text-gray-900 dark:text-white w-20">{props.title}</span>
-                        <span className=" text-gray-900 dark:text-gray-200 max-h-10">Precio unitario</span>
-                            <span className="flex font-bold text-gray-900 dark:text-white justify-around text-sm">$ {props.price}</span>
-                        <span className=" text-gray-900 dark:text-gray-200 max-h-10">Cantidad</span>
-                            <span className=" text-gray-900 dark:text-white">{props.count}</span>
-                        <span className=" text-gray-900 dark:text-gray-200 max-h-10">Total</span>
-                            <span className=" text-gray-900 dark:text-white">$ {props.price * props.count}</span>
-                        <span></span>
-                            <FontAwesomeIcon icon={faTrashCan} className="trash"/> 
-                    </div>
+            <div className='cart-item mobile:w-80 h-auto bg-gray-600 rounded-md p-1 mt-5 flex'>
+                <div className='grilla'>
+                    <table>
+                        <tr className=''> 
+                        <th></th> 
+                            <th scope="col"
+                                        className="px-14 py-3 text-xs font-bold text-gray-500 uppercase  text-center">Producto</th>
+                            <th scope="col"
+                                        className="px-14 py-3 text-xs font-bold text-gray-500 uppercase  text-center">Precio unitario</th>
+                            <th scope="col"
+                                        className="px-14 py-3 text-xs font-bold text-gray-500 uppercase  text-center">Cantidad</th>
+                            <th scope="col"
+                                        className="px-14 py-3 text-xs font-bold text-gray-500 uppercase  text-center">Precio total</th>
+                            <th scope="col"
+                                        className="px-14 py-3 text-xs font-bold text-gray-500 uppercase  text-center"></th>
+                        </tr>
+                        <tr>
+                            <td className="text-sm font-medium text-right whitespace-wrap w-20"><img className="m-2 rounded-sm object-scale-down mt-3 justify-start w-20" src={props.img} alt="product" /></td>
+                            <td className="px-14 py-4 text-sm font-medium whitespace-wrap shrink text-center">{props.title}</td>
+                            <td className="px-14 py-4 text-sm font-medium whitespace-nowrap text-center">$ {props.price}</td>
+                            <td className="px-14 py-4 text-sm font-medium whitespace-nowrap text-center">{props.count}</td>
+                            <td className="px-14 py-4 text-sm font-medium whitespace-nowrap text-center">$ {props.price * props.count}</td>
+                            <td className="px-14 py-4 text-sm font-medium whitespace-nowrap text-center"><FontAwesomeIcon icon={faTrashCan} className="trash"/> </td>
+                        </tr>
+                    </table>
                 </div>
-            </div> */}
-            <table className="cart-item mobile:w-80 h-auto bg-gray-600 rounded-md">
-                <tr>
-                    <th></th>
-                       
-                    <th className='w-20'>Producto</th>
-                    <th>Precio unitario</th>
-                    <th>Cantidad</th>
-                    <th>Precio total</th>
-                    <th></th>
-                </tr>
-                <tr>
-                    <td>
-                        <img className="m-2 w-20 rounded-lg object-scale-down flex mt-3 justify-start" src={props.img} alt="product" />
-                    </td>
-                    <td className='w-20'>{props.title}</td>
-                    <td>$ {props.price}</td>
-                    <td>{props.count}</td>
-                    <td>$ {props.price * props.count}</td>
-                    <td><FontAwesomeIcon icon={faTrashCan} className="trash"/> </td>
-                </tr>
-            </table>
+            </div>
         </>
     )
 }
 
-export default CartItem
+export default CartItem;
